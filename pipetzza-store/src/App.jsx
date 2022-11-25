5d98c1f738b8d72e54802b04b92d683781f0da55
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-11-24 22:47:59 -0300

    Add: inicializar proyecto

---
+++ pipetzza-store/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-red-400">
+        <h1>React App</h1>
+      </div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
