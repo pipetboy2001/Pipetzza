43b1847522931c3032c152e7200d6774bb9df5ff
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-07 13:00:46 -0300

    fix: Precios

--- src/Data2.js
+++ src/Data2.js
@@ -4,7 +4,7 @@ const data = {
     {
       id: 1,
       title: "AMERICANA",
-      price: 401,
+      price: 8690,
       description: "Jamón, Carne, Salchicha, Pepperoni",
       image: "https://i.imgur.com/0dIk4bV.png",
       category: "pizza",
@@ -13,7 +13,7 @@ const data = {
     {
       id: 2,
       title: "BBQ CHICKEN",
-      price: 304,
+      price: 14750,
       description: "Salsa BBQ, Pollo, Cebolla",
       image: "https://i.imgur.com/qTTQ8mB.png",
       category: "pizza",
@@ -22,7 +22,7 @@ const data = {
     {
       id: 3,
       title: "CAMPESINA",
-      price: 475,
+      price: 12050,
       description: "Pollo, Pimiento, Champiñón",
       image: "https://i.imgur.com/GT0RFGP.png",
       category: "pizza",
@@ -31,7 +31,7 @@ const data = {
     {
       id: 4,
       title: " HAWAIANA",
-      price: 399,
+      price: 14750,
       description: "Pollo, Jamón, Piña",
       image: "https://i.imgur.com/T3r2dJ2.png",
       category: "pizza",
@@ -40,7 +40,7 @@ const data = {
     {
       id: 5,
       title: "MECHADA BBQ",
-      price: 405,
+      price: 12405,
       description: "Carne Mechada, Tomate, Cebolla",
       image: "https://i.imgur.com/T0t7syS.png",
       category: "pizza",
@@ -49,7 +49,7 @@ const data = {
     {
       id: 6,
       title: " VEGAN QUEEN",
-      price: 285,
+      price: 10670,
       description: "Champiñón, Pimiento, Cebolla, Choclo y Tomate",
       image: "https://i.imgur.com/OFNZECe.png",
       category: "pizza",
@@ -58,7 +58,7 @@ const data = {
     {
       id: 7,
       title: "Coca Cola",
-      price: 285,
+      price: 1690,
       description: "Refresco",
       image: "https://i.imgur.com/GGDGMVI.png",
       category: "Refresco",
@@ -67,7 +67,7 @@ const data = {
     {
       id: 8,
       title: "Fanta",
-      price: 285,
+      price: 1690,
       description: "Refresco",
       image: "https://i.imgur.com/wzR8Awn.png",
       category: "Refresco",
@@ -76,7 +76,7 @@ const data = {
     {
       id: 9,
       title: "Jugo Watts Durazno",
-      price: 285,
+      price: 1200,
       description: "Refresco",
       image: "https://i.imgur.com/SqKP3KA.png",
       category: "Refresco",
@@ -85,7 +85,7 @@ const data = {
     {
       id: 10,
       title: "Jugo Valle de Piña",
-      price: 285,
+      price: 1200,
       description: "Refresco",
       image: "https://i.imgur.com/K2S4Ilf.png",
       category: "Refresco",
@@ -94,7 +94,7 @@ const data = {
     {
       id: 11,
       title: "Canchantun con Gas",
-      price: 285,
+      price: 1000,
       description: "Refresco",
       image: "https://i.imgur.com/5lsWwYd.png",
       category: "Refresco",
@@ -103,7 +103,7 @@ const data = {
     {
       id: 12,
       title: "Canchantun sin Gas",
-      price: 285,
+      price: 1000,
       description: "Refresco",
       image: "https://i.imgur.com/dMS36DJ.png",
       category: "Refresco",
@@ -113,7 +113,7 @@ const data = {
     {
       id: 13,
       title: "Salsa BBQ",
-      price: 285,
+      price: 600,
       description: "Salsa",
       image: "https://i.imgur.com/qdhBUQ1.png",
       category: "Salsa",
@@ -122,7 +122,7 @@ const data = {
     {
       id: 14,
       title: "Salsa Garlic",
-      price: 285,
+      price: 600,
       description: "Salsa",
       image: "https://i.imgur.com/hBCZd1B.png",
       category: "Salsa",
@@ -131,7 +131,7 @@ const data = {
     {
       id: 15,
       title: " Salsa Picante",
-      price: 285,
+      price: 600,
       description: "Salsa",
       image: "https://i.imgur.com/t9k0Sf8.png",
       category: "Salsa",
@@ -140,7 +140,7 @@ const data = {
     {
       id: 16,
       title: "Salsa Tomate",
-      price: 285,
+      price: 600,
       description: "Salsa",
       image: "https://i.imgur.com/o6zPNEN.png",
       category: "Salsa",
