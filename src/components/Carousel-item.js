c19f43f983b220f19d9c9a5abfbd3611a548bbd9
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-08 22:21:19 -0300

    fix:carrousel link

--- src/components/Carousel-item.js
+++ src/components/Carousel-item.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 
 const HeroSlider = () => {
     return (
         <>
             <Carousel>
                 <Carousel.Item interval={7000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://i.imgur.com/s3M0Xmd.png"
-                        alt="Primera publicidad"
-                    />
+                    <Link to="/Crear">
+                        <img
+                            className="d-block w-100"
+                            src="https://i.imgur.com/s3M0Xmd.png"
+                            alt="Primera publicidad"
+                        />
+                    </Link>
                 </Carousel.Item>
 
                 <Carousel.Item interval={7000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://i.imgur.com/i8FjU66.png"
-                        alt="NotVegan"
-                    />
+                    <Link to="/menu">
+                        <img
+                            className="d-block w-100"
+                            src="https://i.imgur.com/i8FjU66.png"
+                            alt="NotVegan"
+                        />
+                    </Link>
                 </Carousel.Item>
-                
-                
             </Carousel>
         </>
     )
 }
-
 export default HeroSlider
