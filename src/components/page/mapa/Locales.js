57822e83b1845a0bbd63f2a7e4fdc4c0eb5a91ca
Felipe Fernandez <FelipeFelizx@gmail.com>
2022-12-12 16:00:23 -0300

    Fix:Locales

--- src/components/page/mapa/Locales.js
+++ src/components/page/mapa/Locales.js
@@ -6,6 +6,12 @@ import { useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 //css propio para el mapa
 import './Map.css';
+//para que tenga icons
+import { GiPizzaSlice } from 'react-icons/gi';
+import { RiRoadMapLine } from 'react-icons/ri';
+
+
+
 
 
 function LocationMarker() {
@@ -58,8 +64,8 @@ function RenderMap() {
                     <Marker position={positionUsach} icon={icono}>
                         <Popup>
                             <div>
-                                <h3>Pipetzza</h3>
-                                <p>Universidad de Santiago de Chile </p>
+                                <h3>Sede Estacion Central</h3>
+                                <p>Romero 2029, Santiago, Estación Central, Región Metropolitana</p>
                             </div>
                         </Popup>
                     </Marker>
@@ -67,8 +73,8 @@ function RenderMap() {
                     <Marker position={positionMaipu} icon={icono}>
                         <Popup>
                             <div>
-                                <h3>Pipetzza</h3>
-                                <p>Maipu</p>
+                                <h3>Sede maípu</h3>
+                                <p>Provincias Unidas 1337, Maipú, Región Metropolitana</p>
                             </div>
                         </Popup>
                     </Marker>
@@ -86,12 +92,46 @@ function RenderMap() {
 export default function Locales() {
     return (
         <container>
-            <center>
-                <h1>Locales</h1>
-            </center>
-            <div>
-                <RenderMap />
+
+            <div class="angry-grid2">
+                <div id="item-izquierda">
+                    <div>
+                        <RenderMap />
+                    </div>    
+                </div>
+                
+                
+
+                <div id="item-derecha">
+                    <center>
+                        <h1>Nuestros Locales <GiPizzaSlice /> </h1>
+                    </center>
+                    <div className='cards'>
+                    <div className='card'>
+                            <img src="https://th.bing.com/th/id/OIP.EMesVAPaG5ji0vyUcXEfrwHaE7?pid=ImgDet&rs=1" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Sede maípu</h5>
+                            <p class="card-text">Provincias Unidas 1337, Maipú, Región Metropolitana</p>
+                                <a href="#!" class="btn btn-primary">Ubicar <RiRoadMapLine /> </a>
+                        </div>
+                    </div>
+
+                    <div className='card'>
+                            <img src="https://th.bing.com/th/id/OIP.0JN5H7acFTAGwkQNgX6oRAAAAA?pid=ImgDet&w=400&h=266&rs=1" class="card-img-top" alt="Fissure in Sandstone" />
+                        <div class="card-body">
+                            <h5 class="card-title">Sede Estacion Central</h5>
+                                <p class="card-text">Romero 2029, Santiago, Estación Central, Región Metropolitana</p>
+                                <a href="#!" class="btn btn-primary">Ubicar <RiRoadMapLine /> </a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                
             </div>
+
+
+
+            
 
         </container>
     );
